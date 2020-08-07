19deeae63c776a56bebb8fb2592079da1bfde466
Ryan Thom <thomrya22@outlook.com>
2020-08-06 19:25:08 -0500

    created project

---
+++ SpacedRepetition/SpacedRepetition/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SpacedRepetition
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
