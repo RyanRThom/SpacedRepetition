15a0b1acfc480b0eb0ff35d76b2b1237d47e67b5
Ryan Thom <thomrya22@outlook.com>
2020-08-06 20:01:57 -0500

    added card model

---
+++ SpacedRepetition/SpacedRepetition/Models/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpacedRepetition.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string CardFront { get; set; }
+        public string CardBack { get; set; }
+        public int Level { get; set; }
+        public DateTime NextReview { get; set; }
+
+        public Deck Deck { get; set; }
+    }
+}
--- SpacedRepetition/SpacedRepetition/Models/Deck.cs
+++ SpacedRepetition/SpacedRepetition/Models/Deck.cs
@@ -7,5 +7,9 @@ namespace SpacedRepetition.Models
 {
     public class Deck
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Card> Cards { get; set; }
     }
 }
