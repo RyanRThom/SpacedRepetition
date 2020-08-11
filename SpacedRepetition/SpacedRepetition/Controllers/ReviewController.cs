38a465601ebbaeb98503d090613a1967c5451f4d
Ryan Thom <thomrya22@outlook.com>
2020-08-10 19:14:05 -0500

    Added Basic Review functionality

---
+++ SpacedRepetition/SpacedRepetition/Controllers/ReviewController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using SpacedRepetition.Models;
+using SpacedRepetition.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpacedRepetition.Controllers
+{
+    [Authorize]
+    public class ReviewController : Controller
+    {
+        private DeckService _service = new DeckService();
+        public ActionResult Index()
+        {
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            List<Card> model = _service.GetCardsToReview(user);
+            return View(model);
+        }
+
+        public ActionResult Review(int? deckId, int? cardId)
+        {
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            Deck deck = _service.GetDeckById(user, (int)deckId);
+            Card card = _service.GetCardById(user, deck, (int)cardId);
+            card.Level += 1;
+            card.NextReview = FindNextReview(card);
+            _service.EditCard(card);
+            return RedirectToAction("Index");
+        }
+
+        public DateTime FindNextReview(Card card)
+        {
+            int days = FindFib(card.Level);
+            card.NextReview = DateTime.Now;
+            return card.NextReview.AddDays(days);
+        }
+
+        public int FindFib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return FindFib(n - 1) + FindFib(n - 2);
+            }
+        }
+    }
+}
